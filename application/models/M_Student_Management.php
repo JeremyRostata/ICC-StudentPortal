c202e69ab9128bcfd8b78160e82432d207b49c2b
Jasper <garciajapxd@gmail.com>
2024-02-02 23:16:15 +0800

    updates

--- application/models/M_Student_Management.php
+++ application/models/M_Student_Management.php
@@ -11,7 +11,7 @@ class M_Student_Management extends CI_Model{
         $this->db->select('last_name');
         $this->db->select('student_number');
         $this->db->select('student_id');
-        $this->db->select('picture');
+        $this->db->select('profile_name');
         $this->db->from('students');
         return $this->db->get()->result_array();  
     }
