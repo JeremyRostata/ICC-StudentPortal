ccda6b4be57ea640e5efbd4f9cc724b00af1752f
Juanester <86801687+Juanester@users.noreply.github.com>
2024-02-02 11:28:32 +0800

    com

--- application/controllers/C_Employee_Management.php
+++ application/controllers/C_Employee_Management.php
@@ -5,17 +5,23 @@ class C_Employee_Management extends CI_Controller {
 
     public function __construct(){
         parent::__construct();
-        $this->load->model('M_Employee_Management');
+        $this->load->model('M_Employee_Management');  
     }
 
 	public function index()
 	{
         $employee_list = $this->M_Employee_Management->fetchEmployee();
-        $access_role_list = $this->M_Employee_Management->fetchAccessRoles();
-        
+        $access_role_list = $this->M_Employee_Management->fetchAccessRoles();  
+        $employee_count = $this->M_Employee_Management->fetchEmployeeCount();
+        $course_list = $this->M_Employee_Management->fetchCourse();
+        $employee_user_id = $this->M_Employee_Management->fetchEmployeeID();
+
         $data = array(
             'employee_list'=> $employee_list,
-            'access_role_list'=> $access_role_list
+            'employee_count'=> $employee_count,
+            'access_role_list'=> $access_role_list,
+            'course_list'=> $course_list,
+            'employee_user_id' => $employee_user_id,
         );
 
 		$this->load->view('V_Employee_Create', $data);
@@ -25,8 +31,20 @@ class C_Employee_Management extends CI_Controller {
 	{
         $employee_id = $this->input->post('employee_id');
         $access_role_id = $this->input->post('access_role_id');
+
+        $employee_number = $this->input->post('employee_number');
+        $first_name = $this->input->post('first_name');
+        $middle_name = $this->input->post('middle_name');
+        $last_name = $this->input->post('last_name');
+        $course_id = $this->input->post('course_id');
         $password = md5($this->input->post('password'));
 
-        $this->M_Employee_Management->employeeCreate($employee_id, $access_role_id, $password);
+        $this->M_Employee_Management->employeeCreate($employee_id, $access_role_id, $password,$employee_number,$first_name,$middle_name,$last_name,$course_id);
+        redirect(['C_Employee_Management/index']);
 	}
+
+    public function fetchUserID()
+    {
+
+    }
 }
--- application/controllers/C_MIS_Dashboard.php
+++ application/controllers/C_MIS_Dashboard.php
@@ -6,24 +6,29 @@ class C_MIS_Dashboard extends CI_Controller {
     public function __construct(){
         parent::__construct();
         
-        if(empty($_SESSION['employee_id'])){
+        if(empty($_SESSION['employee_id'])){ 
             redirect('C_Employee_Login');
         }
         
-        $this->load->model('M_MIS_Dashboard');
+        $this->load->model('M_MIS_Dashboard'); 
 
     }
 
-    public function index(){
+    public function index(){ 
         
         $employee_id = $this->session->userdata('employee_id');
-        $MIS_info = $this->M_MIS_Dashboard->fetchRegistrarInfo($employee_id);
+        $MIS_info = $this->M_MIS_Dashboard->fetchMISInfo($employee_id);
+        $role_list = $this->M_MIS_Dashboard->fetchAccessRoleId($employee_id); 
+        // $access_role_name = $this->M_MIS_Dashboard->fetchAccessRoles();
         
         $data = array( 
-            'MIS_info' => $MIS_info
+            'MIS_info' => $MIS_info,
+            'role_list'=> $role_list
+            // 'access_role_name'=> $access_role_name
         );
 		$this->load->view('V_MIS_Dashboard', $data);
     }
+
 
     public function logout(){
         $this->session->unset_userdata('employee_id');
