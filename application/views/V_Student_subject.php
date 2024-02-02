2c1f5078cfcc8917fe71501ac14a07d2a6ce99a2
JeremyRostata <earlrostata274@gmail.com>
2024-02-02 15:28:39 +0800

    updated

--- application/views/V_Student_subject.php
+++ application/views/V_Student_subject.php
@@ -31,6 +31,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <nav class="bg-gray-800">
       <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div class="relative flex h-16 items-center justify-between">
+         
 
           <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">            
             <div class="flex flex-shrink-0 items-center">             
@@ -47,7 +48,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">                
             <!-- Profile dropdown -->             
             <div class="relative ml-3">
-
               <div>
                 <button type="button" onclick="toggleProfileDropdown()" class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                   <span class="absolute -inset-1.5"></span>
@@ -56,7 +56,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 </button>
               </div>
 
-              <div id="myProfileDropdown" class="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+              <div id="myProfileDropdown" class="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-blue-200 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                 <!-- Active: "bg-gray-100", Not Active: "" -->
                <span> <?= $student_info['last_name'] . ', ' . $student_info['first_name'] ?>   </span>
                
@@ -67,8 +67,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                           </div>
                           </form>
               </div>
-
-              
               </div>
             </div>         
           </div>
@@ -78,28 +76,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     </nav>
 
 <!-- ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- -->
-
-    
-
-
-    
-    
-        
-
-
-
-
-
-
-
-
         <main class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 mb-20">
-          <h1 class="text-gray-100 mt-5"><span><?= $student_info['course_name'] ?></span></h1>
         <div class="relative flex items-center justify-between">
         <div class=" rounded-lg shadow mt-20">
-          
+          <!-- -----------------------------------------------------------------------------DROPDOWN-------------------------------------------------------------------------------------------------------------------------------- -->
+<div class="relative inline-block text-left">
+  <div>
+    <button type="button" onclick="toggleOptionsDropdown()" class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-blue-200 text-gray-900 px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-800 hover:text-gray-200" id="menu-button" aria-expanded="true" aria-haspopup="true">
+      Options
+      <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+        <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+      </svg>
+    </button>
+  </div>
+  <div id="myOptionsDropdown" class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-blue-200 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+    <div class="py-1" role="none">
+      <a href="http://localhost/C_Student_Dashboard" class="text-gray-900 hover:bg-gray-800 hover:text-white block rounded-md px-3 py-2 text-base font-medium font-mono">DASHBOARD</a>
+          <a href="http://localhost/C_Student_subject" class="text-gray-900 hover:bg-gray-800 hover:text-white block rounded-md px-3 py-2 text-base font-medium font-mono">CHECKLIST</a>
+          <a href="http://localhost/C_Student_fyear" class="text-gray-900 hover:bg-gray-800 hover:text-white block rounded-md px-3 py-2 text-base font-medium font-mono">FIRST YEAR</a>
+          <a href="http://localhost/C_Student_syear" class="text-gray-900 hover:bg-gray-800 hover:text-white block rounded-md px-3 py-2 text-base font-medium font-mono">SECOND YEAR</a>
+          <a href="http://localhost/C_Student_tyear" class="text-gray-900 hover:bg-gray-800 hover:text-white block rounded-md px-3 py-2 text-base font-medium font-mono">THIRD YEAR</a>
+          <a href="http://localhost/C_Student_ftyear" class="text-gray-900 hover:bg-gray-800 hover:text-white block rounded-md px-3 py-2 text-base font-medium font-mono">FOURTH YEAR</a>
+      <form method="POST" action="#" role="none">
+        <button type="submit" class="text-gray-700 block w-full px-4 py-2 text-left text-sm" role="menuitem" tabindex="-1" id="menu-item-3">Sign out</button>
+      </form>
+    </div>
+  </div>
+</div>
+<!-- ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- -->
+ <h1>First Year</h1>
         <section class="flex">
-          <table class="w-full divide-x divide-gray-100">
+          <table class="w-full divide-x divide-gray-100 divide-y divide-gray-800">
         <caption class="bg-gray-200">FIRST SEM</caption>
             <thead class="bg-gray-200 border-b-2 border-gray-200 ">
                 <tr>
@@ -109,7 +116,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 </tr>
             </thead>
 
-            <tbody class="divide-y divide-gray-100">
+            <tbody class="divide-y divide-gray-800">
             <?php foreach ($Fsem1 as $row): ?>
               
                   <tr class="bg-gray-300">
@@ -123,7 +130,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             </tbody>
             </table>
 
-            <table class="w-full divide-x divide-gray-100">
+            <table class="w-full divide-x divide-gray-100 divide-y divide-gray-800">
             <caption class="bg-gray-200">SECOND SEM</caption>
             <thead class="bg-gray-200 border-b-2 border-gray-200 ">
                 <tr>
@@ -137,7 +144,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             </thead>
 
 
-            <tbody class="divide-y divide-gray-100">
+            <tbody class="divide-y divide-gray-800">
             <?php foreach ($Fsem2 as $row): ?>
               
                   <tr class="bg-gray-300">
@@ -153,7 +160,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </section>
             
         <section class="flex mt-2">
-          <table class="w-full divide-x divide-gray-100">
+          <table class="w-full divide-x divide-gray-100 divide-y divide-gray-800">
             <caption class="bg-gray-200">FIRST SEM</caption>
           <thead class="bg-gray-200 border-b-2 border-gray-200 ">
                 <tr>
@@ -163,7 +170,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 </tr>
             </thead>
   
-            <tbody class="divide-y divide-gray-100">
+            <tbody class="divide-y divide-gray-800">
             <?php foreach ($Ssem2 as $row): ?>
               
                   <tr class="bg-gray-300">
@@ -177,7 +184,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             </tbody>
           </table>
 
-          <table class="w-full divide-x divide-gray-100">
+          <table class="w-full divide-x divide-gray-100 divide-y divide-gray-800">
             <caption class="bg-gray-200">SECOND SEM</caption>
            <thead class="bg-gray-200 border-b-2 border-gray-200 ">
                 <tr>
@@ -187,7 +194,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 </tr>
             </thead>
     
-            <tbody class="divide-y divide-gray-100">
+            <tbody class="divide-y divide-gray-800">
             <?php foreach ($Ssem2 as $row): ?>
               
                   <tr class="bg-gray-300">
@@ -203,7 +210,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </section>
 
         <section class="flex mt-2">
-          <table class="w-full divide-x divide-gray-100">
+          <table class="w-full divide-x divide-gray-100 divide-y divide-gray-800">
           <caption class="bg-gray-200">FIRST SEM</caption>
             <thead class="bg-gray-200 border-b-2 border-gray-200 ">
                 <tr>
@@ -213,7 +220,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 </tr>
             </thead>
   
-            <tbody class="divide-y divide-gray-100">
+            <tbody class="divide-y divide-gray-800">
             <?php foreach ($Tsem1 as $row): ?>
               
                   <tr class="bg-gray-300">
@@ -227,7 +234,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             </tbody>
           </table>
 
-          <table class="w-full divide-x divide-gray-100">
+          <table class="w-full divide-x divide-gray-100 divide-y divide-gray-800">
             <caption class="bg-gray-200">SECOND SEM</caption>
           <thead class="bg-gray-200 border-b-2 border-gray-200 ">
                 <tr>
@@ -237,7 +244,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 </tr>
             </thead>
   
-            <tbody class="divide-y divide-gray-100">
+            <tbody class="divide-y divide-gray-800">
             <?php foreach ($Tsem2 as $row): ?>
               
                   <tr class="bg-gray-300">
@@ -253,7 +260,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </section>
         
          <section class="flex mt-2">
-          <table class="w-full divide-x divide-gray-100">
+          <table class="w-full divide-x divide-gray-100 divide-y divide-gray-800">
             <caption class="bg-gray-200">FIRST SEM</caption>
             <thead class="bg-gray-200 border-b-2 border-gray-200 ">
                 <tr>
@@ -263,7 +270,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 </tr>
             </thead>
 
-            <tbody class="divide-y divide-gray-100">
+            <tbody class="divide-y divide-gray-800">
             <?php foreach ($Ftsem1 as $row): ?>
               
                   <tr class="bg-gray-300">
@@ -277,7 +284,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             </tbody>
           </table>
 
-          <table class="w-full divide-x divide-gray-100">
+          <table class="w-full divide-x divide-gray-100 divide-y divide-gray-800">
             <caption class="bg-gray-200">SECOND SEM</caption>
            <thead class="bg-gray-200 border-b-2 border-gray-200 ">
                 <tr>
@@ -287,7 +294,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 </tr>
             </thead>
     
-            <tbody class="divide-y divide-gray-100">
+            <tbody class="divide-y divide-gray-800">
             <?php foreach ($Ftsem2 as $row): ?>
               
                   <tr class="bg-gray-300">
@@ -307,35 +314,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>
         </main>
        
-      
-        
-
-        
-
-
-
-
-
-
-
-        
-        <a href="http://localhost/C_Student_Dashboard">DASHBOARD ></a>
-        <br>
-  <a href="http://localhost/C_Student_subject">CHECKLIST ></a>
-        <br>
-  <a href="http://localhost/C_Student_fyear">FIRST YEAR ></a>
-  <br>
-  <a href="http://localhost/C_Student_syear">SECOND YEAR ></a>
-  <br>
-  <a href="http://localhost/C_Student_tyear">THIRD YEAR ></a>
-  <br>
-  <a href="http://localhost/C_Student_ftyear">FOURTH YEAR ></a>
-        
+   
 
 <!-- -------------------------------------------------------------------------------------JS FOR NAV BAR------------------------------------------------------------------------------------------------------------------------ -->
         <script>
       function toggleProfileDropdown() {
           var dropdown = document.getElementById("myProfileDropdown");
+          dropdown.classList.toggle("hidden");
+      }
+
+      function toggleOptionsDropdown() {
+          var dropdown = document.getElementById("myOptionsDropdown");
           dropdown.classList.toggle("hidden");
       }
       
